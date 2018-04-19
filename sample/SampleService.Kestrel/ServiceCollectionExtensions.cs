1fff6f2cb28511c794b7820bca5fcf55de707a0b
geffzhang <geffzhang@qq.com>
2018-04-19 19:08:42 +0800

    feat: 集成Mediatr

---
+++ sample/SampleService.Kestrel/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NanoFabric.Mediatr;
+
+namespace SampleService.Kestrel
+{
+    /// <summary>
+    /// 应用扩展
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplication<TRequestManager>(
+            this IServiceCollection services, IConfiguration configuration
+        ) where TRequestManager : class, IRequestManager
+        {
+            return services
+                .AddSingleton<IRequestManager, TRequestManager>()
+                .AddSingleton(ApiInfo.Instantiate(configuration));
+        }
+    }
+}
--- sample/SampleService.Kestrel/Startup.cs
+++ sample/SampleService.Kestrel/Startup.cs
@@ -1,5 +1,4 @@
 ﻿using Butterfly.Client.AspNetCore;
-using IdentityServer4.AccessTokenValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
@@ -8,9 +7,15 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NanoFabric.AspNetCore;
 using NanoFabric.AspNetCore.Cors;
+using NanoFabric.Autofac;
+using NanoFabric.Core;
+using NanoFabric.Mediatr;
+using NanoFabric.Mediatr.Autofac;
 using NanoFabric.Router;
+using NanoFabric.Swagger;
 using NLog.Extensions.Logging;
 using Swashbuckle.AspNetCore.Swagger;
+using System;
 using System.Threading;
 
 namespace SampleService.Kestrel
@@ -38,7 +43,7 @@ namespace SampleService.Kestrel
 
         /// This method gets called by the runtime. Use this method to add services to the container.
         /// https://damienbod.com/2018/02/02/securing-an-asp-net-core-mvc-application-which-uses-a-secure-api/
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddNanoFabricConsul(Configuration);
             services.AddNanoFabricConsulRouter();
@@ -47,51 +52,31 @@ namespace SampleService.Kestrel
             services.AddCors();
             services.AddDistributedMemoryCache();
 
-            var authority = Configuration.GetValue<string>("Authority");
-            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
-                .AddIdentityServerAuthentication(options =>
-                {
-                    options.Authority = authority;
-                    options.RequireHttpsMetadata = false;
-                    options.ApiName = "api1";
-                    options.SupportedTokens = SupportedTokens.Both;
-                    options.ApiSecret = "secret";                    
-                });
+            //var authority = Configuration.GetValue<string>("Authority");
+            //services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+            //    .AddIdentityServerAuthentication(options =>
+            //    {
+            //        options.Authority = authority;
+            //        options.RequireHttpsMetadata = false;
+            //        options.ApiName = "api1";
+            //        options.SupportedTokens = SupportedTokens.Both;
+            //        options.ApiSecret = "secret";                    
+            //    });
             services.AddAuthorization(options =>
                    options.AddPolicy("protectedScope", policy =>
-                   {                      
+                   {
                        policy.RequireClaim("scope", "scope_used_for_api_in_protected_zone");
                    })
                );
+            services
+                .AddApplication<InMemoryRequestManager>(Configuration)
+                .AddPermissiveCors()
+                .AddCustomIdentity(ApiInfo.Instance)
+                .AddCustomSwagger(ApiInfo.Instance);
 
             services.AddMvc()
-                .AddMvcApiResult();
-            services.Add(ServiceDescriptor.Transient<ICorsService, WildcardCorsService>());
-            services.Configure<CorsOptions>(options => options.AddPolicy(
-                "AllowSameDomain",builder => builder.WithOrigins("*.*")));
-
-            services.AddOptions();
+               .AddMvcApiResult();
             var collectorUrl = Configuration.GetValue<string>("Butterfly:CollectorUrl");
-            //http://www.cnblogs.com/morang/p/8325729.html
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1",
-                    new Info
-                    {
-                        Version = "v1",
-                        Title = "SampleService 接口文档",
-                        Description = "SampleService 接口集成 Swashbuckle",
-                        TermsOfService = "Values 和 Status（健康检查）.",
-
-                        //Contact = new Contact { Name = "geffzhang", Email = "", Url = "http://github.com/geffzhang" },
-                        //License = new License { Name = "Use under MIT", Url = "https://github.com/geffzhang/NanoFabric/blob/develop/LICENSE.md" }
-                    }
-                         
-                );
-                c.OperationFilter<AssignOperationVendorExtensions>();
-                c.DocumentFilter<ApplyTagDescriptions>();
-                
-            });
 
             services.AddButterfly(option =>
             {
@@ -99,45 +84,87 @@ namespace SampleService.Kestrel
                 option.Service = "SampleService_Kestrel";
                 option.IgnoredRoutesRegexPatterns = new string[] { "/status" };
             });
+
+            return services.ConvertToAutofac(
+                MediatrModule.Create(ApiInfo.Instance.ApplicationAssembly)
+                );
+
+            //services.AddMvc()
+            //    .AddMvcApiResult();
+            //services.Add(ServiceDescriptor.Transient<ICorsService, WildcardCorsService>());
+            //services.Configure<CorsOptions>(options => options.AddPolicy(
+            //    "AllowSameDomain",builder => builder.WithOrigins("*.*")));
+
+            //services.AddOptions();
+            //var collectorUrl = Configuration.GetValue<string>("Butterfly:CollectorUrl");
+            ////http://www.cnblogs.com/morang/p/8325729.html
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1",
+            //        new Info
+            //        {
+            //            Version = "v1",
+            //            Title = "SampleService 接口文档",
+            //            Description = "SampleService 接口集成 Swashbuckle",
+            //            TermsOfService = "Values 和 Status（健康检查）.",
+
+            //            //Contact = new Contact { Name = "geffzhang", Email = "", Url = "http://github.com/geffzhang" },
+            //            //License = new License { Name = "Use under MIT", Url = "https://github.com/geffzhang/NanoFabric/blob/develop/LICENSE.md" }
+            //        }
+                         
+            //    );
+            //    c.OperationFilter<AssignOperationVendorExtensions>();
+            //    c.DocumentFilter<ApplyTagDescriptions>();
+                
+            //});
+
+           
         }
 
         /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime applicationLifetime)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory,
+            IApplicationLifetime applicationLifetime, IApiInfo apiInfo)
         {
-            var log = loggerFactory
-                      .AddNLog()
-                      .CreateLogger<Startup>();
+            loggerFactory.AddNLog();
+            NLog.LogManager.LoadConfiguration("NLog.config");
 
-            loggerFactory.ConfigureNLog("NLog.config");
-            env.EnvironmentName = EnvironmentName.Development;
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/error");
-            }
-            app.UseCors(policy =>
-            {
-                policy.AllowAnyHeader();
-                policy.AllowAnyMethod();
-                policy.WithExposedHeaders("WWW-Authenticate");
-            });
+            app .UseDeveloperExceptionPage()
+                .UsePermissiveCors()
+                .UseCustomSwagger(apiInfo)
+                .UseAuthentication()
+                .UseMvc()
+                .UseStaticFiles()
+                .UseConsulRegisterService(Configuration);
 
-            app.UseAuthentication();
-            app.UseMvc();
-            app.UseStaticFiles();
-            app.UseSwagger(c =>
-             {
-                 c.PreSerializeFilters.Add((swagger, httpReq) => swagger.Host = httpReq.Host.Value);
-             });
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Docs");
+            //env.EnvironmentName = EnvironmentName.Development;
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/error");
+            //}
+            //app.UseCors(policy =>
+            //{
+            //    policy.AllowAnyHeader();
+            //    policy.AllowAnyMethod();
+            //    policy.WithExposedHeaders("WWW-Authenticate");
+            //});
+
+            //app.UseAuthentication();
+            //app.UseMvc();
+            //app.UseStaticFiles();
+            //app.UseSwagger(c =>
+            // {
+            //     c.PreSerializeFilters.Add((swagger, httpReq) => swagger.Host = httpReq.Host.Value);
+            // });
+            //app.UseSwaggerUI(c =>
+            //{
+            //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Docs");
                 
-            });
-            app.UseConsulRegisterService(Configuration);
+            //});
+            //app.UseConsulRegisterService(Configuration);
         }
     }
 }
